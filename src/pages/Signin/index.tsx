c8061f714c4d840a9ed2926d8235394aa8e8d62e
jefersonwork <jefersoncosta336@gmail.com>
2022-09-18 18:18:17 -0300

    create login page

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,5 +1,42 @@
 import React from 'react'
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 
-const SignIn: React.FC = () => <h1>Signin</h1>
+import logoImage from '../../assets/logo.svg'
+
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import { Container, Content, Background } from './styles'
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImage} alt="GoBarber" />
+
+      <form>
+        <h1>Faça seu logon</h1>
+
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Entrar</Button>
+
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+
+      <a href="login">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+    <Background />
+  </Container>
+)
 
 export default SignIn
